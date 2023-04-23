cd21cc932f516c66a2f52182a2b6ff3af2cbda05
Lucas Rosenblatt <lucas.rosenblatt@nyu.edu>
2023-04-23 15:10:42 -0400

    different entropy

--- meal_planning_environment.py
+++ meal_planning_environment.py
@@ -24,26 +24,10 @@ def entropy(labels, base=None):
   https://gist.github.com/jaradc/eeddf20932c0347928d0da5a09298147
   """
 
-  n_labels = len(labels)
-
-  if n_labels <= 1:
-    return 0
-
-  value,counts = np.unique(labels, return_counts=True)
-  probs = counts / n_labels
-  n_classes = np.count_nonzero(probs)
-
-  if n_classes <= 1:
-    return 0
-
-  ent = 0.
-
-  # Compute entropy
+  _,counts = np.unique(labels, return_counts=True)
+  norm_counts = counts / counts.sum()
   base = e if base is None else base
-  for i in probs:
-    ent -= i * log(i, base)
-
-  return ent
+  return -(norm_counts * np.log(norm_counts)/np.log(base)).sum()
 
 def entropy_of_sequence(input_list):
     # get counts
